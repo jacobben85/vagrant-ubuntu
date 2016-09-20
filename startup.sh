2c0597ee316652d88a9661cde4cce4b69d970502
Jacob Benny John <jacob.bennyjohn@boeing.com>
2016-09-20 15:48:13 -0700

    Run scripts as vagrant user

--- startup.sh
+++ startup.sh
@@ -3,3 +3,9 @@
 export JAVA_HOME="/usr/lib/jvm/java-8-oracle"
 
 echo "Added JAVA_HOME"
+
+sudo -H -u vagrant bash -c "touch ~/aps.log"
+sudo -H -u vagrant bash -c "touch ~/pep.log"
+
+sudo -H -u vagrant bash -c "nohup sh /vagrant/aps/aps-express-edition/start_server.sh &> ~/aps.log &"
+sudo -H -u vagrant bash -c "java -jar /vagrant/pep/target/pep.jar &> ~/pep.log &"
